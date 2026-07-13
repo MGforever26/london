88a35f8ec0405a2a4b40473fb8d34d5ce8fa6034
MGforever <guldbrandtsen@gmail.com>
2026-07-13 12:14:13 +0200

    Ret Harry Potter-link til bookingmail med billet-PDF

--- ticket-links.js
+++ ticket-links.js
@@ -1,12 +1,13 @@
 if (window.LONDON_DATA) {
-  window.LONDON_DATA.links.hpTickets = "https://mail.google.com/mail/#all/19dd05fb082f6afb";
+  window.LONDON_DATA.links.hpTickets = "https://mail.google.com/mail/u/0/#inbox/19dd05fb082f6afb";
 
   const monday = window.LONDON_DATA.days.find((day) => day.id === "d13");
   const studioTour = monday?.items.find((item) => item[1] === "Harry Potter Studio Tour");
 
   if (studioTour) {
     studioTour[5] = studioTour[5] || [];
-    const hasTicketLink = studioTour[5].some((link) => link[1] === "hpTickets");
-    if (!hasTicketLink) studioTour[5].unshift(["Billetter i Gmail", "hpTickets"]);
+    studioTour[5] = studioTour[5].filter((link) => link[1] !== "hpTickets");
+    studioTour[5].unshift(["Bookingmail med billet-PDF", "hpTickets"]);
+    studioTour[4] = "Ankomst tidligst kl. 16.10. Åbn bookingmailen via knappen og tryk på den vedhæftede PDF med de fire billetter.";
   }
 }
